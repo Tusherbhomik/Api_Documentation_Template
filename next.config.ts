19062331aec6b634269953711c4abd38ef53a95b
tusher bhomik <2005046@ugrad.cse.buet.ac.bd>
2025-04-17 19:47:15 +0600

    vercel dedicated version

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // Enable static export for GitHub Pages
-  basePath: isProd ? '/Api_Documentation_Template' : '', // Correct basePath with leading slash
-  assetPrefix: isProd ? '/Api_Documentation_Template/' : '', // Ensure assets load correctly
-  images: {
-    unoptimized: true, // Disable image optimization for static export
-  },
 };
 
 export default nextConfig;
